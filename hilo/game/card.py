989ed751733e4baea93ec08f5f3559aad9cc9fbe
extraordinaria <v.bardalesvelsquez@gmail.com>
2022-01-22 18:48:31 -0500

    add compare_card fuction _----veronica

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -4,7 +4,21 @@ class Card:
 
     def __init__(self):
         self.value_card = 0
+        self.value_card_2 = 0
+        self.h_l_card = ""
 
 
     def show_card(self):
         self.value_card = random.randint(1,13) 
+        self.value_card_2 = random.randint(1,13)
+
+        while self.value_card_2 == self.value_card:
+             self.value_card_2 = random.randint(1,13)
+
+    def compare_card(self):
+        if self.value_card_2 > self.value_card:
+            self.h_l_card = "h"
+        else:
+            self.h_l_card = "l"
+        
+
