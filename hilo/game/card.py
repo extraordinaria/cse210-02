bf24b5d963ed08a3ff5b16e1c94eb5fedd954c76
extraordinaria <v.bardalesvelsquez@gmail.com>
2022-02-03 20:48:03 -0500

    .

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,28 +1,18 @@
 import random
-
 class Card:
-
     def __init__(self):
         self.value_card = random.randint(1,13)
-        self.value_card_2 = 0
-        self.h_l_card = ""
-        
-
-
-    def show_card(self):
-        
-        
-        self.value_card_2 = random.randint(1,13)
-
-        while self.value_card_2 == self.value_card:
-            self.value_card_2 = random.randint(1,13)
-
-        if self.value_card < self.value_card_2:
-            self.h_l_card = "h"
-        elif self.value_card > self.value_card_2:
-            self.h_l_card = "l"
-
-        self.value_card = self.value_card_2
+        self.value = 0
+        if self.value_card == 1:
+            self.value = "A"
+        elif self.value_card == 11:
+            self.value = "J"
+        elif self.value_card == 12:
+            self.value = "Q"
+        elif self.value_card == 13:
+            self.value = "K"
+        else:
+            self.value = self.value_card
 
         
         
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,75 +1,62 @@
 from game.card import Card
-
-
 class Director:
-
-
+    '''
+    The director of the HiLo game.
+    '''
     def __init__(self):
-
         self.hilo = Card()
+        self.current_value = self.hilo.value_card
         self.is_playing = True
-        self.score = 0
         self.total_score = 300
+        self.new_card_value = -1
         self.guess_card = ""
-        self.hilo.show_card()
-        self.card_number = self.hilo.value_card
 
     def start_game(self):
-
-
         while self.is_playing:
-
             self.do_body_game()
             self.show_score()
             self.get_inputs()
-
+            self.current_value = self.new_card_value
+    
     def do_body_game(self):
-
         if not self.is_playing:
             return
-
-        
-        self.hilo.show_card()  
-        print (f"The card is: {self.hilo.value_card}")
-        #print (f"The card is: {self.card_number}")
-
+        print (f"The card is: {self.current_value}")
         self.guess_card = input("Higher or lower? [h/l] ")
-        while self.guess_card != "h" and self.guess_card != "l":
-            self.guess_card = input("You have enter a correct answer: higher or lower? [h/l] ")
-        
-
-
+        while self.guess_card.lower() != "h" and self.guess_card.lower() != "l":
+            self.guess_card = input("You have entered an incorrect answer: higher or lower? [h/l] ")
+    
     def show_score(self):
-
         if not self.is_playing:
             return
-
-        self.hilo.show_card()
-        self.next_card = self.hilo.value_card_2
-        print(f"Next card was: {self.next_card}")
-
-
-        if self.hilo.h_l_card == self.guess_card:
-            self.score = 100
-        else:
-            self.score = -75
-        self.total_score += self.score
-        print(f"Your score is {self.total_score}")
+        # While the cards are the same value, pick a new card instead
+        the_same = True
+        while the_same:
+            self.next_card = Card()
+            if self.next_card.value_card == self.current_value:
+                the_same = True
+            else:
+                the_same = False
         
-
+        print(f"Next card was: {self.next_card.value}")
+        self.new_card_value = self.next_card.value_card
+        if self.guess_card.lower() == 'h' and (self.current_value < self.new_card_value):
+            self.total_score += 100
+        elif self.guess_card.lower() == 'l' and (self.current_value > self.new_card_value):
+            self.total_score += 100
+        else:
+            self.total_score -= 75
+        print(f"Your score is {self.total_score}")
 
     def get_inputs(self):
-
         if self.total_score > 0:
             ask_player = input("Play again? [y/n] ")
-            while ask_player != "y" and ask_player != "n":
-                ask_player = input("You have enter a correct answer: Play again? [y/n] ")
+            while ask_player.lower() != "y" and ask_player.lower() != "n":
+                ask_player = input("You have entered an incorrect answer: Play again? [y/n] ")
             self.is_playing = (ask_player == "y")
             print()
-            self.card_number = self.hilo.value_card_2
         else:
             self.is_playing = False
-            
 
         
 
